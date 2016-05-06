17d0830622d6797a5a78e38696639f100174b79c
Kwon, oh-sang <cetauri@gmail.com>
2016-05-06 14:00:28 +0900

    add Click Event for nodes

--- js/mf_network.js
+++ js/mf_network.js
@@ -14,7 +14,7 @@
         var eDate = list.attr("eDate");
         // console.log(sDate, eDate, count, score);
 
-        data_nodes = [{id: 0, label: '', group : 0}];
+        data_nodes = [{id: 0, label: '', group : 0, title : score}];
         data_edges = [];
 
         if ($(xml).find("List").find("Member").length > 0) {
@@ -25,25 +25,28 @@
               var c_score = $(this).attr("score");
               var children = $(this).clone();
               children.find("Child").each(function(i) {
-              // Child 노드 추가
-              // console.log(idx, i, $(this).text());
-              var sub_idx = id + '.' + i;
-              data_nodes.push({id: id + '.' + i, label: $(this).text(), group : 2, title : c_score});
-              data_edges.push({from: id, to: sub_idx});
+                  var c_text = $(this).text();
+              
+                  // Child 노드 추가
+                  // console.log(idx, i, $(this).text());
+                  var sub_idx = id + '.' + i;
+                  data_nodes.push({id: id + '.' + i, label: c_text, group : 2, title : c_score});
+                  data_edges.push({from: id, to: sub_idx});
 
+                  $("body").data('' + sub_idx , c_text );
+              });
+
+            $(this).contents().empty(); 
+            var m_text = $(this).text().trim();
+            var m_score = $(this).attr("score");
+            // console.log(idx, m_score, m_text);
+
+            //main 노드 추가
+            data_nodes.push({id: id, label: m_text, group : 1, title : m_score});
+            data_edges.push({from: 0, to: id});
+            
+            $("body").data('' + id , m_text );
           });
-
-          $(this).contents().empty(); 
-          var m_text = $(this).text().trim();
-          var m_score = $(this).attr("score");
-          // console.log(idx, m_score, m_text);
-
-          //main 노드 추가
-          data_nodes.push({id: id, label: m_text, group : 1, title : m_score});
-          data_edges.push({from: 0, to: id});
-
-        });
-
         }
         // console.log(data_nodes);
 
@@ -54,8 +57,19 @@
         };
 
         var network = new vis.Network(container, data, options);
+        network.on("doubleClick", function (params) {
+          var node_id = params.nodes[0];
 
-      }, 
+          if (node_id == 0){
+            return;
+          }
+
+          var value = $("body").data(node_id);
+          if (value !== ""){
+              alert(value);
+          }
+        });
+      },
       error: function(xhr, status, error) {
         alert(error); 
       }

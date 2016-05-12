dadfa14fad36c13c7c937c51c424982642f04256
Kwon, oh-sang <cetauri@gmail.com>
2016-05-12 21:24:04 +0900

    for production

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,21 @@
+  function getQueryParams(qs) {
+      qs = qs.split('+').join(' ');
+
+      var params = {},
+          tokens,
+          re = /[?&]?([^=]+)=([^&]*)/g;
+
+      while (tokens = re.exec(qs)) {
+          params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+      }
+
+      return params;
+  }
+
+  function getParameter(name){
+    var query = getQueryParams(document.location.search);
+    return query[name];
+  }
 
   function draw_word_network(element_id, url, type, options) {
 
